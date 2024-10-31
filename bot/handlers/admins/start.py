7a4456cca00139372effbbac01a21cfcf0f0894c
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2024-11-01 04:59:19 +0500

    Update start.py

--- bot/handlers/admins/start.py
+++ bot/handlers/admins/start.py
@@ -68,7 +68,7 @@ async def show_requests(call: types.CallbackQuery):
             await call.message.delete()
             await call.message.answer(requests_message, reply_markup=keyboard)
         else:
-            await call.message.edit_text("Arizalar topilmadi.")
+            await call.message.edit_text("Arizalar topilmadi.", reply_markup=admin_menu_btn)
     
     except Exception as e:
         logger.exception(f"Error in show_requests: {e}")
